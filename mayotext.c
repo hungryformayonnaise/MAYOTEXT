035c223c15de2b01d97c75f79dcbdbfc946f6abb
hungryformayo <hungryformayo@gmail.com>
2025-04-04 16:20:20 +0200

    minor bug fixed
    Signed-off-by: hungryformayo <hungryformayo@gmail.com>

--- mayotext.c
+++ mayotext.c
@@ -286,13 +286,13 @@ void editorInsertChar(int c) {
 
 void editorInsertNewline() {
 	if(E.cx==0) {
-		editorInsertRow(E.cy. "". 0);
+		editorInsertRow(E.cy, "", 0);
 	} else {
 		erow*row=&E.row[E.cy];
 		editorInsertRow(E.cy+1, &row->chars[E.cx], row->size-E.cx);
 		row=&E.row[E.cy];
 		row->size=E.cx;
-		row*>chars[row->size]='\0';
+		row->chars[row->size]='\0';
 		editorUpdateRow(row);
 	}
 	E.cy++;
@@ -452,7 +452,7 @@ void editorProcessKeypress() {
 	
 	switch(c) {
 		case '\r':
-			editorInsertNewline;
+			editorInsertNewline();
 			break;
 		case CTRL_KEY('q'):
 			if(E.dirty&&quit_times>0) {
